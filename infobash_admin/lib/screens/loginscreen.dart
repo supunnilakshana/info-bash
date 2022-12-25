c7087700a98d7c0da9ae16dc2ee6e3437af8c837
hashanranasinghe <hashan.ranasinghe98@gmail.com>
2022-12-25 23:28:31 +0530

    check signin

--- infobash_admin/lib/screens/loginscreen.dart
+++ infobash_admin/lib/screens/loginscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:infobash_admin/screens/auth/check_signIn.dart';
 import 'package:infobash_admin/screens/requestscreen.dart';
 import 'package:infobash_admin/services/auth/signin_mannager.dart';
 
@@ -26,57 +27,61 @@ class _LogInScreenState extends State<LogInScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Form(
-        key: _form,
-        child: Padding(
-          padding: EdgeInsets.only(
-              left: size.width * 0.1,
-              right: size.width * 0.1,
-              top: size.height * 0.5),
-          child: Column(
-            children: [
-              Gtextformfiled(
-                label: "Email of Team leader",
-                onchange: (text) {
-                  email = text;
-                },
-                controller: emailController,
-                save: (text) {
-                  email = text!;
-                },
-                valid: (text) {
-                  return Validater.vaildemail(text!);
-                },
-              ),
-              Gpasswordformfiled(
-                  textinput: TextInputType.text,
-                  icon: Icons.key,
-                  onchange: (text) {
-                    password = text;
-                  },
-                  save: (text) {
-                    password = text!;
-                  }),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: size.height * 0.02, bottom: size.height * 0.02),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Genaralbutton(
-                      onpress: () {
-                        SigninManager().signIn(email, password).then((value) =>
-                            Navigator.of(context)
-                                .pushReplacementNamed(RequestScreen.routName));
+      body: Center(
+        child: Form(
+          key: _form,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: size.width * 0.1,
+                  right: size.width * 0.1,
+                  ),
+              child: Column(
+                children: [
+                  Gtextformfiled(
+                    label: "Email",
+                    onchange: (text) {
+                      email = text;
+                    },
+                    controller: emailController,
+                    save: (text) {
+                      email = text!;
+                    },
+                    valid: (text) {
+                      return Validater.vaildemail(text!);
+                    },
+                  ),
+                  Gpasswordformfiled(
+                      textinput: TextInputType.text,
+                      icon: Icons.key,
+                      onchange: (text) {
+                        password = text;
                       },
-                      text: "Register",
-                      pleft: size.width * 0.2,
-                      pright: size.width * 0.2,
-                    )
-                  ],
-                ),
-              )
-            ],
+                      save: (text) {
+                        password = text!;
+                      }),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: size.height * 0.02, bottom: size.height * 0.02),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Genaralbutton(
+                          onpress: () {
+                            SigninManager().signIn(email, password).then((value) =>
+                                Navigator.of(context)
+                                    .pushReplacementNamed(CheckSignIn.routName));
+                          },
+                          text: "Register",
+                          pleft: size.width * 0.2,
+                          pright: size.width * 0.2,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
