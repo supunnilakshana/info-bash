399a7e7b0eb2a88812205297d7710edb301e8c50
SupunAppri <supunn@appritechnologies.com>
2023-01-21 12:17:08 +0530

    WTP: match deati

--- infobash_view/lib/screens/match_details_screen.dart
+++ infobash_view/lib/screens/match_details_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, unnecessary_brace_in_string_interps
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
@@ -27,15 +27,26 @@ class _MatchDetailsScreenState extends State<MatchDetailsScreen> {
   late Stream matchStream;
   late Stream ballStream;
   late String ballpath;
+  int score1 = 0;
+  int wickets1 = 0;
+  double runrate1 = 0.0;
+  int score2 = 0;
+  int wickets2 = 0;
+  double runrate2 = 0.0;
+  int over1 = 0;
+  int ball1 = 0;
+  int over2 = 0;
+  int ball2 = 0;
+
   @override
   void initState() {
-    // futurematchdata = FbHandeler.getMatchModel(widget.matchid);
+    ballpath = CollectionPath.ballpath(widget.matchid);
     matchStream = FirebaseFirestore.instance
         .collection(CollectionPath.matchpath)
         .doc(widget.matchid)
         .snapshots();
     ballStream = FirebaseFirestore.instance.collection(ballpath).snapshots();
-    ballpath = CollectionPath.ballpath(widget.matchid);
+
     super.initState();
   }
 
@@ -45,55 +56,231 @@ class _MatchDetailsScreenState extends State<MatchDetailsScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("More"),
+        title: const Text("More"),
         toolbarHeight: size.height * 0.09,
         backgroundColor: kPrimaryColordark,
         actions: [
           Image.asset("assets/icons/app_icon.png"),
         ],
       ),
-      body: StreamBuilder<dynamic>(
-          stream: matchStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              DocumentSnapshot documentSnapshot =
-                  snapshot.data as DocumentSnapshot;
-              var model = MatchModel.fromMap(documentSnapshot.id,
-                  documentSnapshot.data() as Map<String, dynamic>);
-              model.toMap();
-              return Column(
-                children: [
-                  Text(model.datetime),
-                  StreamBuilder(
-                    stream: ballStream,
-                    builder: (context, snapshotball) {
-                      if (snapshot.hasData) {
-                        return Text('Something went wrong');
-                      }
-                      if (snapshot.hasError) {
-                        return Text('Something went wrong');
-                      }
-                      return Center(child: CircularProgressIndicator());
-                    },
-                  )
+      body: SizedBox(
+        width: size.width,
+        child: StreamBuilder<dynamic>(
+            stream: matchStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                DocumentSnapshot documentSnapshot =
+                    snapshot.data as DocumentSnapshot;
+                var matchModel = MatchModel.fromMap(documentSnapshot.id,
+                    documentSnapshot.data() as Map<String, dynamic>);
+                matchModel.toMap();
 
-                  // ListView.builder(
-                  //     itemBuilder: (context, index) {
-                  //       setState(() {});
+                List<BallModel> balllist = [];
+                if (matchModel.matchstatus == Matchstatustype.notstared) {
+                  return Text("Match is not started");
+                } else {
+                  return Column(
+                    children: [
+                      Text(matchModel.datetime),
+                      StreamBuilder(
+                        stream: ballStream,
+                        builder: (context, snapshotball) {
+                          if (snapshotball.hasData) {
+                            balllist = [];
+                            List<BallModel> ining1ballst = [];
+                            List<BallModel> ining2ballst = [];
+                            score1 = 0;
+                            wickets1 = 0;
+                            runrate1 = 0.0;
+                            score2 = 0;
+                            wickets2 = 0;
+                            runrate2 = 0.0;
+                            over1 = 0;
+                            ball1 = 0;
+                            over2 = 0;
+                            ball2 = 0;
 
-                  //       return Text("id.toString()");
-                  //     },
-                  //     itemCount: matchViewModel.ballModel.length,
-                  //     scrollDirection: Axis.vertical,
-                  //     shrinkWrap: true)
-                ],
-              );
-            }
-            if (snapshot.hasError) {
-              return Text('Something went wrong');
-            }
-            return const Center(child: CircularProgressIndicator());
-          }),
+                            BallModel ballmodel;
+                            QuerySnapshot querySnapshot =
+                                snapshotball.data as QuerySnapshot;
+                            for (int i = 0;
+                                i < querySnapshot.docs.length;
+                                i++) {
+                              var a = querySnapshot.docs[i];
+                              ballmodel = BallModel.fromMap(
+                                  a.data() as Map<String, dynamic>);
+                              balllist.add(ballmodel);
+                            }
+
+                            for (var element in balllist) {
+                              if (element.matchid == matchModel.inning1) {
+                                ining1ballst.add(element);
+                              }
+                            }
+
+                            for (var element in balllist) {
+                              if (element.matchid == matchModel.inning2) {
+                                ining2ballst.add(element);
+                              }
+                            }
+
+                            // balllist.sort((a, b) => b.matchid.compareTo(a.matchid));
+                            ining1ballst
+                                .sort((a, b) => b.matchid.compareTo(a.matchid));
+                            ining2ballst
+                                .sort((a, b) => b.matchid.compareTo(a.matchid));
+
+                            for (var element in ining1ballst) {
+                              score1 += element.totalmark;
+                              if (element.wickettype != "") {
+                                wickets1++;
+                              }
+                            }
+                            for (var element in ining2ballst) {
+                              score2 += element.totalmark;
+                              if (element.wickettype != "") {
+                                wickets2++;
+                              }
+                            }
+                            if (ining1ballst.isNotEmpty) {
+                              over1 = ining1ballst.last.overno;
+                              ball1 = ining1ballst.last.bno;
+                            }
+                            if (ining2ballst.isNotEmpty) {
+                              over2 = ining2ballst.last.overno;
+                              ball2 = ining2ballst.last.bno;
+                            }
+                            return Expanded(
+                              child: Column(
+                                children: [
+                                  Column(
+                                    children: [
+                                      if (matchModel.matchstatus ==
+                                          Matchstatustype.ongoning)
+                                        Column(
+                                          children: [
+                                            matchModel.inning1s ==
+                                                        Matchstatustype
+                                                            .ongoning ||
+                                                    matchModel.inning1s ==
+                                                        Matchstatustype.end
+                                                ? Text(
+                                                    "$score1/$wickets1   $over1.$ball1")
+                                                : Container(),
+                                            matchModel.inning2s ==
+                                                        Matchstatustype
+                                                            .ongoning ||
+                                                    matchModel.inning2s ==
+                                                        Matchstatustype.end
+                                                ? Text(
+                                                    "$score2/$wickets2 $over2.$ball2")
+                                                : Container(),
+                                          ],
+                                        )
+                                      else if (matchModel.matchstatus ==
+                                          Matchstatustype.end)
+                                        Column(
+                                          children: [
+                                            Text(
+                                                "inig 1   ${score1}/${wickets1}"),
+                                            Text(
+                                                "inig 2   ${score2}/${wickets2}"),
+                                            Text(
+                                                "winn team   ${score2}/${wickets2}"),
+                                          ],
+                                        ),
+                                    ],
+                                  ),
+                                  Expanded(
+                                    child: DefaultTabController(
+                                      length: 2,
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Container(
+                                            color: Colors.blue,
+                                            constraints: BoxConstraints.expand(
+                                                height: 50),
+                                            child: TabBar(tabs: [
+                                              Tab(text: "Inning 1"),
+                                              Tab(text: "Inning 2"),
+                                            ]),
+                                          ),
+                                          Expanded(
+                                            child: TabBarView(children: [
+                                              if (ining1ballst.isNotEmpty)
+                                                ListView.builder(
+                                                  itemCount:
+                                                      ining1ballst.length,
+                                                  itemBuilder:
+                                                      (context, index) {
+                                                    var ballmodel1 =
+                                                        ining1ballst[index];
+                                                    return ListTile(
+                                                      title: Text(
+                                                          "${ballmodel1.overno} . ${ballmodel1.bno} "),
+                                                    );
+                                                  },
+                                                )
+                                              else
+                                                Container(
+                                                  child: Text("No data"),
+                                                ),
+                                              if (ining2ballst.isNotEmpty)
+                                                ListView.builder(
+                                                  itemCount:
+                                                      ining2ballst.length,
+                                                  itemBuilder:
+                                                      (context, index) {
+                                                    var ballmodel2 =
+                                                        ining2ballst[index];
+                                                    return ListTile(
+                                                      title: Text(
+                                                          "${ballmodel2.overno} . ${ballmodel2.bno} "),
+                                                    );
+                                                  },
+                                                )
+                                              else
+                                                Container(
+                                                  child: Text("No data"),
+                                                ),
+                                            ]),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                          if (snapshotball.hasError) {
+                            return const Text('Something went wrong');
+                          }
+                          return const Center(
+                              child: const CircularProgressIndicator());
+                        },
+                      )
+
+                      // ListView.builder(
+                      //     itemBuilder: (context, index) {
+                      //       setState(() {});
+
+                      //       return Text("id.toString()");
+                      //     },
+                      //     itemCount: matchViewModel.ballModel.length,
+                      //     scrollDirection: Axis.vertical,
+                      //     shrinkWrap: true)
+                    ],
+                  );
+                }
+              }
+              if (snapshot.hasError) {
+                return const Text('Something went wrong');
+              }
+              return const Center(child: CircularProgressIndicator());
+            }),
+      ),
     );
   }
 }
